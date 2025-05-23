4bfd36349a422cb56f77ebcbee94f3232ca67cb2
Atem Kuot <atemliaikuot@gmail.com>
2025-05-23 15:33:59 +0300

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,12 @@
-let input2 = prompt("Say something:");
-    if (input2 === "") {
+const args = process.argv.slice(2);
+let count = 0;
+for (const arg of args) {
+    count++;
+      }
+     
+    if (count === 0) {
         console.log("No argument")}
+    
     else {
-        console.log(input2)
+        console.log(args)
         }
