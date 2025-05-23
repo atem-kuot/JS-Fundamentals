9cf6b17ed0193bd5001588e69fb5c4acc4ff48b3
Atem Kuot <atemliaikuot@gmail.com>
2025-05-23 22:20:15 +0300

    Update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,7 +1,12 @@
 const firstArg = process.argv[2];
 const secondArg = process.argv[3];
 
+const num1 = parseInt(firstArg);
+const num2 = parseInt(secondArg);
 
 function add(a, b) {
-    console.log(firstArga + secondArg);
+    console.log(a + b);
 }
+
+if (!isNaN(num1) && !isNaN(num2)) {
+    add(firstArga, secondArg )}
