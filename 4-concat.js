ab6031f28a834d012617bd384def8b7e3a3f1c17
Atem Kuot <atemliaikuot@gmail.com>
2025-05-23 20:15:02 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,10 +1,4 @@
-const firstArg = process.argv.slice(2);
-const secondArg = process.argv.slice(3);
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
 
-if (firstArg === undefined) {
-    firstArg = undefined}
-else if (secondArg === undefined) {
-    secondArg = undefined}
-
-    
 console.log(`${firstArg} is ${secondArg}`);
