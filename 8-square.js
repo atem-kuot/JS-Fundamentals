fba186f0b348efc89542657b3e6d6d1a8752135d
Atem Kuot <atemliaikuot@gmail.com>
2025-05-23 21:55:36 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,12 +1,10 @@
-const firstArg = process.argv[2];
-
 const num = parseInt(firstArg);
 let i = 0;
 while (num > i){
 if (!isNaN(num)) {
-    console.log("X" * num);
+    console.log("X".repeat(num));
 } else {
-    console.log('Missing number of occurrences');
+    console.log('Missing size');
 }
 i++;
 }
