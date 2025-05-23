add781cf5b17f21e7367e9c9dfab0ae5b565a305
Atem Kuot <atemliaikuot@gmail.com>
2025-05-23 21:29:45 +0300

    Add 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,12 +2,12 @@ const firstArg = process.argv[2];
 
 const num = parseInt(firstArg);
 let i = 0;
-while (num > i)
-if (!isNaN(num) {
+while (num > i){
+if (!isNaN(num)) {
     console.log("C is fun");
 } else {
     console.log('Missing number of occurrences');
 }
 i++;
-
+}
 
